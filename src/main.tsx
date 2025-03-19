a684450137cf9e6b139d315d949d53090b229e2f
Umais Siddiqui <umaismuhammad99@gmail.com>
2025-03-19 16:43:15 +0500

    m

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,5 @@
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom';
 import App from './App.tsx'
 import './index.css'
 
-createRoot(document.getElementById("root")!).render(
-
-    <BrowserRouter basename="/umaismsiddiqui.github.io">
-    <App />
-  </BrowserRouter>
-);
+createRoot(document.getElementById("root")!).render(<App />);
--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/umaismsiddiqui.github.io/',
+  base: '/umaismsiddiqui.github.io',
   server: {
     host: "::",
     port: 8080,
